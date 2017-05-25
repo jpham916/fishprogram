84595ec6dc1113f0a5cafea9b1748b9b627f3b1d
jpham916 <jpham@bren.ucsb.edu>
2017-05-25 15:44:16 -0700

    Making progress on fishStats

--- fishStats.R
+++ fishStats.R
@@ -6,7 +6,15 @@ fishStats <- function(price, frequency, showGraph){
   Fishlocations <- read.csv(frequency, header= TRUE)
   
   mergedfish <- merge(Fishlocations,FishPrices,by=c ("Fish"))
-  
   mergedfish
-  
+
+alaska <- as.character(mergedfish[which.max(mergedfish$Alaska),1])
+ff <- as.character(mergedfish[which.max(mergedfish$Frewfarms),1])
+tt <- as.character(mergedfish[which.max(mergedfish$Taguetown),1])
+
+mostabundant <- c(alaska,ff,tt)
+placeNames <- c("Alaska","Frew Farms", "Tague Town")
+abundTable <- cbind(placeNames, mostabundant)
+colnames(abundTable) <- c("Location","Fish")
+
 }
